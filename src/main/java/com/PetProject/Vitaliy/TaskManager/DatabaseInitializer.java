99a7906e83a59fe26d34149dcc49dcf7a55b5c46
V.Sazonov <VitaliySazonov20@gmail.com>
2025-08-15 15:00:33 +0400

    scheduled reset after midnight

--- src/main/java/com/PetProject/Vitaliy/TaskManager/DatabaseInitializer.java
+++ src/main/java/com/PetProject/Vitaliy/TaskManager/DatabaseInitializer.java
@@ -2,7 +2,9 @@ package com.PetProject.Vitaliy.TaskManager;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DatabaseInitializer {
@@ -19,4 +21,11 @@ public class DatabaseInitializer {
 
         databaseDataInitializer.initTestData();
     }
+
+    @Scheduled(cron = "0 1 0 * * ?")
+    public void afterDatabaseResetReInit() {
+        databaseSchemaInitializer.initializeSchema();
+        databaseDataInitializer.initTestData();
+    }
+
 }
---
+++ src/main/java/com/PetProject/Vitaliy/TaskManager/Service/DatabaseResetService.java
@@ -0,0 +1,44 @@
+package com.PetProject.Vitaliy.TaskManager.Service;
+
+import com.PetProject.Vitaliy.TaskManager.DatabaseDataInitializer;
+import com.PetProject.Vitaliy.TaskManager.DatabaseSchemaInitializer;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class DatabaseResetService {
+
+    @Autowired
+    private DatabaseSchemaInitializer databaseSchemaInitializer;
+
+    @Autowired
+    private DatabaseDataInitializer databaseDataInitializer;
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    @Transactional
+    public void resetDatabase(){
+
+        entityManager.createNativeQuery("DELETE FROM task_comments").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM tasks").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM user_credentials").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM users").executeUpdate();
+        entityManager.createNativeQuery("DELETE FROM audit_log").executeUpdate();
+
+        databaseSchemaInitializer.initializeSchema();
+
+        databaseDataInitializer.initTestData();
+
+        entityManager.clear();
+
+    }
+
+}
